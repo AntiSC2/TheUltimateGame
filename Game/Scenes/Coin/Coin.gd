208bde8e9deca61c0b10133d2382b458d4b87471
Jakob Sinclair <jakob.sinclair99@gmail.com>
2015-10-21 22:03:12 +0200

    Added Win Scenario.
    Added a win node that shows up when you pick up the coin.

--- Game/Scenes/Coin/Coin.gd
+++ Game/Scenes/Coin/Coin.gd
@@ -1,2 +1,12 @@
 
+extends Area2D;
+
+func _on_Coin_body_enter( body ):
+	if(body.get_name() == "Player"):
+		queue_free();
+
+func _ready():
+	pass
+
+
 
--- Game/Scenes/Levels/Level1.gd
+++ Game/Scenes/Levels/Level1.gd
@@ -1,10 +1,10 @@
 
-extends CollisionShape2D
+extends Node2D
 
 func _fixed_process(delta):
-	pass
+	if(get_child_count() < 4):
+		get_node("YouWin").show();
 
 func _ready():
 	set_fixed_process(true);
-
 
