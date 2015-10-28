9f259d38506441bc760cf6cbdf734082516a341b
Jakob Sinclair <99JAS001@LK-S02356.lulea.se>
2015-10-28 12:37:02 +0100

    Improved win conditions.

--- Game/Scenes/Levels/Level1.gd
+++ Game/Scenes/Levels/Level1.gd
@@ -1,10 +1,13 @@
 
 extends Node2D
 
+var children_count;
+
 func _fixed_process(delta):
-	if(get_child_count() < 4):
+	if(get_child_count() < children_count):
 		get_node("YouWin").show();
 
 func _ready():
+	children_count = get_child_count();
 	set_fixed_process(true);
 
